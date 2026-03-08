53e9541ea25f16484e2274b5f8a0e01c9cd76376
Juan J. Lorenzo Sáez <juan-lorenzo@hotmail.es>
2026-03-08 10:01:35 +0100

    CaS Initial Project Commit.

---
+++ src/main/java/com/culturaalsur/CulturaAlSurApplication.java
@@ -0,0 +1,13 @@
+package com.culturaalsur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturaAlSurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturaAlSurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/culturaalsur/CulturaAlSurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culturaalsur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturaAlSurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
